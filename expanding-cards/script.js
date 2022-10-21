a6a707c75a9645139d3a8bae8fc66013f4d109a6
刘松 <lvez@easestrategy.com>
2022-10-21 17:41:17 +0800

    feat:expanding-card

---
+++ expanding-cards/script.js
@@ -0,0 +1,9 @@
+const ctnEl = document.querySelector('.container')
+
+ctnEl.addEventListener('click', function (event) {
+  Array.prototype.forEach.call(this.children, (el) => {    
+    el.classList.remove('active')
+  })
+
+  event.target.classList.add('active')
+})
