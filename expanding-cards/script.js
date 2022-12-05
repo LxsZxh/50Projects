989948c7a275e5f071eeebcb310d34445fb11bf0
刘松 <1476273138@qq.com>
2022-12-05 09:42:49 +0800

    fix(expanding-cards):格式修改

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,9 +1,9 @@
 const ctnEl = document.querySelector('.container')
 
 ctnEl.addEventListener('click', function (event) {
-  Array.prototype.forEach.call(this.children, (el) => {    
+  Array.prototype.forEach.call(this.children, (el) => {   
+    console.log(this.children) 
     el.classList.remove('active')
   })
-
   event.target.classList.add('active')
 })
