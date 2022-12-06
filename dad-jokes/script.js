a153fbcc78d71e4a9f418215d234327e7fae028a
刘松 <1476273138@qq.com>
2022-12-06 11:48:52 +0800

    fix(index):填充50projects项目名字

--- dad-jokes/script.js
+++ dad-jokes/script.js
@@ -12,7 +12,6 @@ async function generateJoke(){
         }
     }
     const res=await fetch('https://icanhazdadjoke.com',config)
-    console.log(res)
     const data =await res.json()
     jokeEl.innerHTML=data.joke
 }
--- incrementing-counter/script.js
+++ incrementing-counter/script.js
@@ -2,7 +2,7 @@ const counters = document.querySelectorAll('.counter')
 counters.forEach(counter => {
   counter.innerText = '0'
   const updateCounter = () => {
-    const target = +counter.getAttribute('data-target')
+    const target = +counter.getAttribute('data-target')    
     const c = +counter.innerText
     const increment = target / 200
     if (c < target) {
