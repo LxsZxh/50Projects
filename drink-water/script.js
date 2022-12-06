4b7c4b778ed464d5c91787493055ee5a7d71c3ab
刘松 <1476273138@qq.com>
2022-12-06 20:18:49 +0800

    feat(movie-app):电影应用

--- drink-water/script.js
+++ drink-water/script.js
@@ -10,14 +10,18 @@ smallCups.forEach((cup, idx) => {
 })
 
 function highlightCups(idx) {
+  // 点击两次最后一个则索引减1
   if (idx === 7 && smallCups[idx].classList.contains('full')) {
     idx--
-  } else if (
+  }
+  // 当前被点一次了且后面一杯水没被点,在点一次自身就会索引-1
+  else if (
     smallCups[idx].classList.contains('full') &&
     !smallCups[idx].nextElementSibling.classList.contains('full')
   ) {
     idx--
   }
+  // 小于等于当前索引值的增加full类，否则移除
   smallCups.forEach((cup, idx2) => {
     if (idx2 <= idx) {
       cup.classList.add('full')
@@ -30,7 +34,7 @@ function highlightCups(idx) {
 
 function updateBigCup() {
   const fullCups = document.querySelectorAll('.cup-small.full').length
-  const totalCups = smallCups.length
+  const totalCups = smallCups.length 
   
   if (fullCups === 0) {
     percentage.style.visibility = 'hidden'
