7f1bb4519f3a7302878de16824cb964d09053309
刘松 <1476273138@qq.com>
2022-12-12 11:51:58 +0800

    feat(toast-notification):吐司通知

---
+++ toast-notification/script.js
@@ -0,0 +1,28 @@
+const button = document.getElementById('button')
+const toasts = document.getElementById('toasts')
+
+const messages = ['消息一', '消息二', '消息三', '消息四']
+
+const types = ['info', 'success', 'error']
+
+button.addEventListener('click', () => createNotification())
+
+function createNotification(message = null, type = null) {
+  const notif = document.createElement('div')
+  notif.classList.add('toast')
+  notif.classList.add(type ? type : getRandomType())
+  notif.innerText = message ? message : getRandomMessage()
+
+  toasts.appendChild(notif)
+  setTimeout(() => {
+    notif.remove()
+  }, 3000)
+}
+
+function getRandomType() {
+  return types[Math.floor(Math.random() * types.length)]
+}
+
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)]
+}
