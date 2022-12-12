033b0ca945fc812486c09b1d5aa2155c83b1aea5
刘松 <1476273138@qq.com>
2022-12-12 20:03:51 +0800

    feat(good-cheap-fast):好/便宜/快

---
+++ good-cheap-fast/script.js
@@ -0,0 +1,25 @@
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelector('#good')
+console.log('good: ', good)
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
+
+toggles.forEach(toggle =>
+  toggle.addEventListener('change', e => doTheTrick(e.target))
+)
+
+function doTheTrick(theClickOne) {
+  if (good.checked && cheap.checked && fast.checked) {
+    if (good === theClickOne) {
+      fast.checked = false
+    }
+
+    if (cheap == theClickOne) {
+      good.checked = false
+    }
+
+    if (fast === theClickOne) {
+      cheap.checked = false
+    }
+  }
+}
