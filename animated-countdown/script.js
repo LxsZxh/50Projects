5091ae34988676d628d5090a6902a6a177d94412
刘松 <1476273138@qq.com>
2022-12-13 11:57:25 +0800

    feat(animated-countdown):动画倒计时

---
+++ animated-countdown/script.js
@@ -0,0 +1,42 @@
+const nums = document.querySelectorAll('.nums span')
+const counter = document.querySelector('.counter')
+const finalMessage = document.querySelector('.final')
+const replay = document.querySelector('#replay')
+
+runAnimation()
+
+function resetDOM() {
+  counter.classList.remove('hide')
+  finalMessage.classList.remove('show')
+  nums.forEach(num => {
+    num.classList.value = ''
+  })
+  nums[0].classList.add('in')
+}
+
+function runAnimation() {
+  nums.forEach((num, idx) => {
+    const nextToLast = nums.length - 1
+    num.addEventListener('animationend', e => {
+      // 当前使用goIn动画且不是最后一个元素
+      if (e.animationName === 'goIn' && idx !== nextToLast) {
+        num.classList.remove('in')
+        num.classList.add('out')
+      }
+      // 当前使用goOut动画且下一个不为空
+      else if (e.animationName === 'goOut' && num.nextElementSibling) {
+        num.nextElementSibling.classList.add('in')
+      }
+      // 最后一个元素
+      else {
+        counter.classList.add('hide')
+        finalMessage.classList.add('show')
+      }
+    })
+  })
+}
+
+replay.addEventListener('click', () => {
+  resetDOM()
+  runAnimation()
+})
