bbe03af168d6f19f4b1fcf5b87c8ddfd3808800d
刘松 <1476273138@qq.com>
2022-12-13 16:23:40 +0800

    fix(pokedex):去掉console.log

--- pokedex/script.js
+++ pokedex/script.js
@@ -29,7 +29,6 @@ const getPokemon = async id => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`
   const res = await fetch(url)
   const data = await res.json()
-  console.log('data: ', data);
   createPokemonCard(data)
 }
 
